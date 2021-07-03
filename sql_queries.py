f9d523215eb64db270cf691ec8a2939ad1e00336
Matheus Felipe dos Anjos <anjos97.matheus@gmail.com>
2021-07-02 23:46:39 -0300

    dwh.cfg file added to gitignore

--- sql_queries.py
+++ sql_queries.py
@@ -5,22 +5,64 @@ import configparser
 config = configparser.ConfigParser()
 config.read('dwh.cfg')
 
+s3_config = config["S3"]
+log_data_s3_path = s3_config.get("LOG_DATA")
+song_data_s3_path = s3_config.get("SONG_DATA")
+log_json_s3_path = s3_config.get("LOG_JSONPATH")
+
+iam_config = config["IAM_ROLE"]
+arn = iam_config.get("ARN")
+
 # DROP TABLES
 
-staging_events_table_drop = ""
-staging_songs_table_drop = ""
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs"
+songplay_table_drop = "DROP TABLE IF EXISTS songplay"
+user_table_drop = "DROP TABLE IF EXISTS user"
+song_table_drop = "DROP TABLE IF EXISTS song"
+artist_table_drop = "DROP TABLE IF EXISTS artist"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
 staging_events_table_create= ("""
+    CREATE TABLE IF NOT EXISTS staging_events(
+        event_id BIGINT IDENTITY PRIMARY KEY NOT NULL,
+        artist VARCHAR(255),
+        auth VARCHAR(255),
+        firstName VARCHAR(255),
+        gender VARCHAR (1),
+        itemInSession INT,
+        lastName VARCHAR(255),
+        length DOUBLE PRECISION,
+        level VARCHAR(255),
+        location VARCHAR(255),
+        method VARCHAR(30),
+        page VARCHAR(30),
+        registration VARCHAR(255),
+        sessionId BIGINT,
+        song VARCHAR(255),
+        status INT,
+        ts VARCHAR(255),
+        userAgent VARCHAR(255),
+        userId VARCHAR(255)
+    )
+
 """)
 
 staging_songs_table_create = ("""
+    CREATE TABLE IF NOT EXISTS staging_songs(
+        song_id VARCHAR(255) PRIMARY KEY,
+        num_songs INT,
+        title VARCHAR(255),
+        artist_name VARCHAR(255),
+        artist_latitude DOUBLE PRECISION,
+        year INT,
+        duration DOUBLE PRECISION,
+        artist_id VARCHAR(255),
+        artist_longitude DOUBLE PRECISION,
+        artist_location VARCHAR(255)
+    )
 """)
 
 songplay_table_create = ("""
@@ -40,11 +82,24 @@ time_table_create = ("""
 
 # STAGING TABLES
 
-staging_events_copy = ("""
-""").format()
+staging_events_copy = (f"""
+    COPY
+        staging_events 
+    FROM
+        {log_data_s3_path}
+    iam_role {arn}
+    json {log_json_s3_path}
+""")
 
-staging_songs_copy = ("""
-""").format()
+staging_songs_copy = (f"""
+    COPY
+        staging_songs
+    FROM
+        {song_data_s3_path}
+    iam_role {arn}
+    json 'auto'
+""")
+print(staging_songs_copy)
 
 # FINAL TABLES
 
